16f70323abebf6caa8990d77b289605ecc1b9c27
Ujwal <ujjwal1141@gmail.com>
2023-09-06 13:03:23 +0530

    removed profile in backend

--- provider.tf
+++ provider.tf
@@ -5,11 +5,10 @@ terraform {
       version = "~> 5.0"
     }
   }
-    backend "s3" {
-    bucket = "test-s3-state"
-    key    = "terraform.tfstate"
-    region = "us-west-2"
-    profile = "jenkins"
+  backend "s3" {
+    bucket  = "test-s3-state"
+    key     = "terraform.tfstate"
+    region  = "us-west-2"
   }
 }
 
