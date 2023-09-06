b7540f55e428ebb91a7479e96269bf1c0b9ec0e5
Ujwal <ujjwal1141@gmail.com>
2023-09-06 23:33:55 +0530

    added alb

---
+++ modules/alb_module/main.tf
@@ -0,0 +1,23 @@
+resource "aws_security_group" "alb" {
+  name        = "terraform_alb_security_group"
+  description = "Terraform ALB security group"
+  vpc_id      = var.vpc_id
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags= {
+    Name = "terraform-example-alb-security-group"
+  }
+}
