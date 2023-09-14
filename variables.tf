76aa9c820417783c45df2be47cb06277b5bd62f9
yoU <ujjwal1141@gmail.com>
2023-09-14 19:17:30 +0530

    variables

--- variables.tf
+++ variables.tf
@@ -1,6 +1,9 @@
 variable "my_public_ip" {
   description = "public ip cidr"
   type        = string
+  default = "ec2"
 }
 
-variable "instance_name" {}
+variable "instance_name" {
+  default = "0.0.0.0/0"
+}
